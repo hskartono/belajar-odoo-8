f6835cee9f98da90c9fec66ad7f0e46452c2cb99
hskartono <hskartono@gmail.com>
2022-12-07 10:21:41 +0700

    first commit

---
+++ todo_app/__openerp__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Harry S. Kartono',
+    'depends': ['mail'],
+    'application': True,
+    'data': ['todo_view.xml'],
+}
