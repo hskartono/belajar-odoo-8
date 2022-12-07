eae91f53dc7269c94b2fb7ae7547e9ad21fe6d79
hskartono <hskartono@gmail.com>
2022-12-07 13:44:19 +0700

    menambahkan fitur ke dalam fungsi todo

---
+++ multiuser_todo_app/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Multiuser To-Do',
+    'description': 'Extend the To-Do app to multiuser.',
+    'author': 'Harry S. Kartono',
+    'depends': ['todo_app'],
+    'application': True,
+    'data': [
+        'views/todo_view.xml',
+    ],
+}
